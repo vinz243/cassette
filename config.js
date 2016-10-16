a707b0a8eeb587f79f295d9f8ec2fdd7e026bcf6
vinz243 <vinz243@gmail.com>
2016-10-16 13:13:19 +0200

    remove local config and create fallback

--- config.js
+++ config.js
@@ -1,14 +1,22 @@
 
 import process from 'process';
 import shortid from 'shortid';
-
-import localConfig from './config.local.json';
+import os from 'os';
 
 if (!process.argv.includes('--dev')) {
   var env = 'test';
 } else {
   var env = 'dev';
 }
+let localConfig = {};
+try {
+  localConfig = require('./config.local.json');
+} catch (err) {
+  if (env !== 'production') {
+    localConfig[env].rootDir = os.homedir();
+  }
+}
+
 env = localConfig.env || env;
 
 
