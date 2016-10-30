45639221e94ee2a00a5af89f83db18372e272632
vinz243 <vinz243@gmail.com>
2016-10-30 12:17:40 +0100

    feat(models): simple ORM

---
+++ src/server/models/Model.js
@@ -0,0 +1,203 @@
+import Datastore from 'nedb-promise';
+import conf from '../../../config.js';
+import config from './config.js';
+
+import mkdirp from 'mkdirp';
+import Lazy from 'lazy.js';
+
+import pascalCase from 'pascal-case';
+import snakeCase from 'snake-case';
+
+
+mkdirp.sync(conf.rootDir + '/data/')
+
+class ModelField {
+  constructor(name, model) {
+    this.name = name;
+    this.model = model;
+  }
+  int() {
+    this.type = 'int';
+    this.validator = (data) => {
+      let value = data;
+      if (data === parseInt(data, 10)) {
+        return true;
+      }
+      return false;
+    }
+    return this;
+  }
+  float() {
+    this.type = 'float';
+    this.validator = (n) => Number(n) === n;
+    return this;
+  }
+  string() {
+    this.type = 'string';
+    this.validator = (data) => typeof data === 'string';
+    return this;
+  }
+  boolean() {
+    this.type = 'boolean';
+    this.validator = (data) => typeof data === 'boolean';
+    return this;
+  }
+  oneToOne() {
+    this.type = 'oneToOne';
+    this.validator = (data) => typeof data === 'string';
+    return this;
+  }
+  regex(reg) {
+    if(this.type !== 'string')
+      throw new Error('Using regex on a non-string field');
+    return this;
+  }
+  required() {
+    this._required = true;
+    return this;
+  }
+  notIdentity() {
+    this._notIdentity = true;
+    return this;
+  }
+  noDuplicates() {
+    this._noDuplicates = true;
+    return this;
+  }
+  done() {
+    return this.model;
+  }
+}
+class Model {
+  constructor(name) {
+    this.name = name;
+    this.dbName = snakeCase(name);
+    this.dbPath = conf.rootDir + '/data/' + this.dbName + '.db';
+    this.fields = [];
+
+    this.field('_id').string();
+  }
+  field(name) {
+    let field = new ModelField(name, this);
+    this.fields.push(field);
+    return field;
+  }
+  acceptsEmptyQuery() {
+    this.acceptsEmptyQuery = true;
+    return this;
+  }
+  done() {
+    let self = this;
+
+    let db = new Datastore(self.dbPath);
+    db.loadDatabase();
+
+    let model = function (d) {
+      this.data = {};
+      // let data = Lazy(d).pick(this.fields.map(f => f.name));
+      for (let index in self.fields) {
+        let field = self.fields[index];
+        let value = d[field.name];
+        this.data[field.name] = value;
+      }
+    }
+
+    model.prototype.getPayload = function () {
+      let payload = {};
+      // console.log(self.fields);
+      for (let index in self.fields) {
+        let field = self.fields[index];
+        let value = this.data[field.name];
+
+        // console.log(field, value);
+
+        if (typeof value === 'undefined' && field._required)
+          throw new Error(`Field ${field.name} is required`);
+
+        if (field.validator && !field.validator(value)) {
+          if (field._required)
+            throw new Error(`Field ${field.name} is required but has invalid value`);
+          // else console.log('    Warning: dropping ' + field.name + ' as ' + value);
+        } else {
+          payload[field.name] = value;
+        }
+      }
+      return payload;
+    }
+
+    model.prototype.create = async function () {
+      // console.log(this.getPayload());
+      let res = await db.insert(this.getPayload());
+
+      this._id = res._id;
+    }
+
+    model.prototype.set = function (key, value) {
+      if (key === '_id')
+        throw new Error('Cannot set _id');
+      this.data[key] = value;
+    }
+
+    model.prototype.update = async function (key, value) {
+      if (!this._id)
+        throw new Error('Cannot update ghost model');
+      await db.update({_id: this._id}, this.getPayload());
+      return;
+    }
+
+    model.findById = async function (id) {
+      return (await model.find({_id: id, limit: 1}))[0];
+    }
+
+    model.find = async function (query, forceEmpty) {
+      let q = Lazy(query)
+        .omit(self.fields.filter(f => f.notIdentity))
+        .pick(self.fields.map(f => f.name))
+        // .concat(['sort', 'skip', 'limit', 'direction'])
+        .value();
+
+      if (Object.keys(q).length  == 0 && !self.acceptsEmptyQuery && !forceEmpty)
+        throw new Error('Empty or invalid query');
+
+
+      let opts = Lazy(query).pick([
+        'limit', 'offset', 'sort', 'direction'
+      ]).value();
+
+      if (!opts.limit || opts.limit > 25 || opts.limit < 1) {
+        opts.limit = 25;
+      }
+
+      let sort = {};
+      sort[opts.sort || 'name'] = opts.direction ?
+        (opts.direction == 'asc' ? 1 : -1) : 1;
+      let res = (await db.cfind(q).sort(sort).limit(opts.limit)
+        .skip(opts.skip || 0).exec()).map(d => new model(d));
+      res.query = Lazy(q).merge(opts);
+      return res;
+    }
+    // for (let i in this.fields.filter(t => t.type == 'oneToOne')) {
+    //   let field = this.fields[i];
+    //   model.prototype['get' + pascalCase(field.name)] = {
+    //
+    //   }
+    // }
+    return model;
+  }
+}
+
+// Chaining example:
+//
+// (new Model('MyModel'))
+//   .field('itemName')
+//     .required()
+//     .string()
+//     .regex(/Regex/)
+//     .done()
+//   .field('itemCategory')
+//     .float()
+//     .notIdentity() <-- means won't be accepted as a query
+//     .done()
+//   .done()
+
+export default Model;
---
+++ tests/server/unit/Model.test.js
@@ -0,0 +1,152 @@
+import Model from '../../../src/server/models/Model';
+import test from 'ava';
+
+test('constructor should snake case db name', t => {
+  t.is((new Model('myDoc')).dbName, 'my_doc');
+})
+
+test('field should type correctly to float', t => {
+  let m = new Model('myModel');
+  let floatField = m.field('foo').float();
+
+  t.is(floatField.type, 'float');
+
+  t.falsy(floatField.validator('foo'));
+  t.falsy(floatField.validator('e'));
+  t.falsy(floatField.validator());
+  t.falsy(floatField.validator({}));
+  t.falsy(floatField.validator(NaN));
+  t.falsy(floatField.validator(function() {}));
+
+  t.truthy(floatField.validator(15));
+  t.truthy(floatField.validator(42.1));
+  t.truthy(floatField.validator(-1337));
+});
+
+test('field should type correctly to int', t => {
+  let m = new Model('myModel');
+  let intField = m.field('foo').int();
+
+  t.is(intField.type, 'int');
+
+  t.falsy(intField.validator('foo'));
+  t.falsy(intField.validator('e'));
+  t.falsy(intField.validator());
+  t.falsy(intField.validator({}));
+  t.falsy(intField.validator(NaN));
+  t.falsy(intField.validator(function() {}));
+  t.falsy(intField.validator(42.1));
+
+  t.truthy(intField.validator(15));
+  t.truthy(intField.validator(-1337));
+});
+
+test('field should type correctly to string', t => {
+  let m = new Model('myModel');
+  let stringField = m.field('foo').string();
+
+  t.is(stringField.type, 'string');
+
+  t.truthy(stringField.validator('foo'));
+  t.truthy(stringField.validator('e'));
+
+  t.falsy(stringField.validator());
+  t.falsy(stringField.validator({}));
+  t.falsy(stringField.validator(NaN));
+  t.falsy(stringField.validator(function() {}));
+  t.falsy(stringField.validator(42.1));
+});
+
+test('field should type correctly to bool', t => {
+  let m = new Model('myModel');
+  let boolField = m.field('foo').boolean();
+
+  t.is(boolField.type, 'boolean');
+
+  t.falsy(boolField.validator('foo'));
+  t.falsy(boolField.validator());
+  t.falsy(boolField.validator({}));
+  t.falsy(boolField.validator(NaN));
+  t.falsy(boolField.validator(function() {}));
+
+  t.truthy(boolField.validator(true));
+});
+
+
+test('should chain calls without Error', t => {
+  (new Model('MyModel'))
+    .field('itemName')
+      .required()
+      .string()
+      .noDuplicates()
+      .done()
+    .field('itemCategory')
+      .float()
+      .notIdentity() // <-- means won't be accepted as a query
+      .done()
+    .acceptsEmptyQuery()
+    .done()
+});
+let personModel = (name) => {
+  return (new Model(name))
+    .field('name')
+      .required()
+      .string()
+      .done()
+    .field('age')
+      .int()
+      .done()
+    .done();
+}
+
+test('model should generate correct payloads', t => {
+
+  let Person = personModel('People');
+  let bilbo = new Person({
+    name: 'Bilbo',
+    age: 131
+  });
+  t.deepEqual(bilbo.getPayload(), {
+    name: 'Bilbo',
+    age: 131
+  });
+
+  t.throws(function () {
+      return (new Person({})).getPayload();
+  });
+
+  t.deepEqual((new Person({
+    name: 'Aragorn',
+    age: 121.2,
+    height: 175
+  })).getPayload(), {
+    name: 'Aragorn'
+  });
+
+  t.throws(function () {
+    (new Person({name: 42})).getPayload();
+  });
+});
+test('model should create() correctly and then find it by id and name', async t => {
+  let Person = personModel('Individuals');
+
+  let bilbo = new Person({
+    name: 'Bilbo',
+    age: 131
+  });
+
+  await bilbo.create();
+
+  let frodo = new Person({name: 'Frodo'});
+  await frodo.create();
+
+  t.not(bilbo._id, undefined);
+
+  let res = await Person.findById(bilbo._id);
+  t.is(res.data.age, 131);
+  t.is(res.data.name, 'Bilbo');
+
+  res = (await Person.find({name: 'Bilbo'}))[0];
+  t.is(res.data._id, bilbo._id);
+  t.is(bilbo.data.age, 131);
+})
