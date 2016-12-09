2dde2257c03ac68abe47279b6753870d1cb20d2f
vinz243 <vinz243@gmail.com>
2016-12-09 21:55:42 +0100

    chore(config): remove useless lines

--- src/server/models/config.js
+++ src/server/models/config.js
@@ -4,19 +4,8 @@ import mkdirp from 'mkdirp';
 
 mkdirp.sync(conf.rootDir + '/data/')
 
-// console.log('  Using dir ' + conf.rootDir);
 let db = new datastore(conf.rootDir + '/data/config.db');
 db.loadDatabase();
-
-const DB_ERROR_STATUS = {
-  status: 500,
-  success: false,
-  data: {
-    error_code: 'EINTERNAL',
-    error_message: 'Internal error. Try again later'
-  }
-};
-
 
 const model = {
 
@@ -38,39 +27,34 @@ const model = {
     return (await model.getValue(key)).value || defVal;
   },
   updateValue: async (key, value) => {
-    try {
-      // console.log(key);
-      let found = await db.find({key: key});
+    // console.log(key);
+    let found = await db.find({key: key});
 
-      // console.log('found', found);
-      if(found.length == 0) {
-        return {
-          success: false,
-          status: 404,
-          data: {
-            error_message: 'Cannot find key',
-            error_code: 'ENOTFOUND'
-          }
-        };
-      }
-
-      await db.update({
-        key: key
-      }, {
-        key: key,
-        value: value
-      });
-
+    // console.log('found', found);
+    if(found.length == 0) {
       return {
-        status: 200,
-        success: true,
-        data: {}
-      }
-
-    } catch (err) {
-      console.log(err);
-      return DB_ERROR_STATUS;
+        success: false,
+        status: 404,
+        data: {
+          error_message: 'Cannot find key',
+          error_code: 'ENOTFOUND'
+        }
+      };
     }
+
+    await db.update({
+      key: key
+    }, {
+      key: key,
+      value: value
+    });
+
+    return {
+      status: 200,
+      success: true,
+      data: {}
+    }
+
   },
   /*
    Inserts a value to database. Return complex object for API
@@ -80,26 +64,21 @@ const model = {
       key: key,
       value: value
     };
-    try {
-      if((await db.find({key: key})).length > 0) {
-        return {
-          success: false,
-          status: 400,
-          data: {
-            error_message: 'A config entry already exists with this key',
-            error_code: 'EDUPENTRY'
-          }
-        };
-      }
-      await db.insert(doc);
+    if((await db.find({key: key})).length > 0) {
       return {
-        status: 201,
-        success: true,
-        data: {}
-      }
-    } catch (err) {
-      console.log(err);
-      return DB_ERROR_STATUS;
+        success: false,
+        status: 400,
+        data: {
+          error_message: 'A config entry already exists with this key',
+          error_code: 'EDUPENTRY'
+        }
+      };
+    }
+    await db.insert(doc);
+    return {
+      status: 201,
+      success: true,
+      data: {}
     }
 
   }
