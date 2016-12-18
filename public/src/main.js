efae93b436035593eb41c6fea15fd3e534ea709c
vinz243 <vinz243@gmail.com>
2016-12-18 17:33:04 +0100

    feat(public): add skeleton

---
+++ public/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+new Vue({
+  el: '#main',
+  data: {
+    message: "Hello Vue"
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // This is the "main" file which should include all other modules
+  entry: './public/src/main.js',
+  // Where should the compiled file go?
+  output: {
+    // To the `dist` folder
+    path: './public/dist',
+    // With the filename `build.js` so it's dist/build.js
+    filename: 'build.js'
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  }
+}
