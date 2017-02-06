db72b11d3e080c00a7157eb94a83ef5012f1cfc9
vinz243 <vinz243@gmail.com>
2017-02-06 19:49:39 +0100

    feat: add artwork caching system

--- src/server/features/artworks/endpoint.js
+++ src/server/features/artworks/endpoint.js
@@ -1,9 +1,17 @@
 import {getClosestSize} from './sizes';
 import {Album, Artist} from '../../models';
+import config from '../../config.js';
 import qs from 'querystring';
+import mkdirp from 'mkdirp';
+import path from 'path';
 import request from 'request-promise-native';
 import {mainStory} from 'storyboard';
 import chalk from 'chalk';
+import md5 from 'md5';
+import fs from 'fs';
+
+const dataDir = path.join(config.get('configPath'), '/cache/');
+mkdirp.sync(dataDir);
 
 export default {
   '/v1/albums/:id/art': {
@@ -14,10 +22,35 @@ export default {
       if (!album)
         return ctx.throw(404);
 
+      const artist = await Artist.findById(album.data.artistId);
+
+      let hash = md5(qs.stringify({
+        action: 'get_album_art',
+        size: size,
+        id: album._id
+      }));
+
+      let cachePath = path.join(dataDir, hash);
+      if (fs.existsSync(cachePath)) {
+        ctx.body = await (new Promise((resolve, reject) => {
+          return fs.readFile(cachePath, (err, data) => {
+            if (err) {
+              mainStory.error('artwork', `reading '${cachePath}' failed`, {attach: err});
+              return reject(err);
+            }
+            resolve(data);
+          })
+        }));
+        mainStory.info('artwork', 'sending cached album artwork');
+        ctx.status = 200;
+        ctx.set('Content-Type', 'image/png');
+        return;
+      }
+
       const params = {
         method: 'album.getinfo',
         api_key: '85d5b036c6aa02af4d7216af592e1eea',
-        artist: (await Artist.findById(album.data.artistId)).data.name,
+        artist: artist.data.name,
         album: album.data.name,
         format: 'json'
       };
@@ -36,11 +69,19 @@ export default {
 
       ctx.status = 200;
       ctx.set('Content-Type', 'image/png')
-      ctx.body = await request({
+      let buffer = await request({
         url: imageUrl, encoding: null
       });
       mainStory.info('artwork', `GET ${chalk.dim(imageUrl)} - ${Date.now() - time}ms`);
-
+      ctx.body = buffer;
+      fs.writeFile(cachePath, buffer, (err) => {
+        if (err) {
+          return mainStory.error('artwork', `could not write to cache artwork (${cachePath})`, {
+            attach: err
+          });
+        }
+        return mainStory.info('artwork', 'cached artwork for next time');
+      });
     }
   },
   '/v1/artists/:id/art': {
@@ -51,6 +92,28 @@ export default {
       if (!artist)
         return ctx.throw(404);
 
+      let hash = md5(qs.stringify({
+        action: 'get_artist_art',
+        size: size,
+        id: artist._id
+      }));
+
+      let cachePath = path.join(dataDir, hash);
+      if (fs.existsSync(cachePath)) {
+        ctx.body = await (new Promise((resolve, reject) => {
+          return fs.readFile(cachePath, (err, data) => {
+            if (err) {
+              mainStory.error('artwork', `reading '${cachePath}' failed`, {attach: err});
+              return reject(err);
+            }
+            resolve(data);
+          })
+        }));
+        mainStory.info('artwork', 'sending cached album artwork');
+        ctx.status = 200;
+        ctx.set('Content-Type', 'image/png');
+        return;
+      }
       const params = {
         method: 'artist.getinfo',
         api_key: '85d5b036c6aa02af4d7216af592e1eea',
@@ -75,11 +138,19 @@ export default {
       ctx.set('Content-Type', 'image/png');
 
       const time2 = Date.now();
-      ctx.body = await request({
+      let buffer = await request({
         url: imageUrl, encoding: null
       });
       mainStory.info('artwork', `GET ${chalk.dim(imageUrl)} - ${Date.now() - time2}ms`);
-
+      ctx.body = buffer;
+      fs.writeFile(cachePath, buffer, (err) => {
+        if (err) {
+          return mainStory.error('artwork', `could not write to cache artwork (${cachePath})`, {
+            attach: err
+          });
+        }
+        return mainStory.info('artwork', 'cached artwork for next time');
+      });
     }
   }
 }
