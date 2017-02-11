ab318c5cdce1105b256457e2005f1f8a8bc24cf1
vinz243 <vinz243@gmail.com>
2017-02-12 00:13:21 +0100

    feat: better model implementation

--- src/server/models/Model.js
+++ src/server/models/Model.js
@@ -7,7 +7,8 @@ import assert from 'assert';
 import events from 'events';
 import mkdirp from 'mkdirp';
 import path from 'path';
-import Lazy from 'lazy.js';
+import Boom from 'boom';
+import assignWith from 'lodash/assignWith';
 
 import pascalCase from 'pascal-case';
 import pluralize from 'pluralize';
@@ -16,306 +17,120 @@ import snakeCase from 'snake-case';
 let dataDir = path.join(conf.get('configPath'), '/data/');
 mkdirp.sync(dataDir);
 
-class ModelField {
-  constructor(name, model) {
-    this.name = name;
-    this.model = model;
-  }
-  int() {
-    this.type = 'int';
-    this.validator = (data) => {
-      let value = data;
-      if (data === parseInt(data, 10)) {
-        return true;
-      }
-      return false;
-    }
-    return this;
-  }
-  defaultParam () {
-    this._default = true;
-    this.model._default = this.name;
-    return this;
-  }
-  defaultValue(val) {
-    this._defaultValue = val;
-    return this;
-  }
-  float() {
-    this.type = 'float';
-    this.validator = (n) => Number(n) === n;
-    return this;
-  }
-  string() {
-    this.type = 'string';
-    this.validator = (data) => typeof data === 'string';
-    return this;
-  }
-  boolean() {
-    this.type = 'boolean';
-    this.validator = (data) => typeof data === 'boolean';
-    return this;
-  }
-  oneToOne() {
-    this.type = 'oneToOne';
-    this.validator = (data) => typeof data === 'string';
-    return this;
-  }
-  any() {
-    this.type = 'any';
-    this.validator = (data) => true;
-    return this;
-  }
-  regex(reg) {
-    if(this.type !== 'string')
-      throw new Error('Using regex on a non-string field');
-    return this;
-  }
-  required() {
-    this._required = true;
-    return this;
-  }
-  notIdentity() {
-    this._notIdentity = true;
-    return this;
-  }
-  done() {
-    return this.model;
+let databases = {};
+// this.dbPath = path.join(dataDir, this.dbName + '.db');
+
+// type State = {
+//   db: any,
+//   dirty: boolean,
+//   props: any,
+//   fields: string[]
+// }
+
+const notImplemented = (name) => {
+  return function () {
+    throw new Boom.create(500, `Function ${name} isn't implemented yet`);
   }
 }
 
-let databases = {};
+// export const baseModel = (props) => {
+//   let state: State = {
+//     db: {},
+//     dirty: false,
+//     fields: Array.isArray(fields) ? fields : Object.keys(props),
+//     _props: props
+//   }
+//   let defaultFunctions =
+// };
 
-class Model {
-  constructor(name) {
-    this.name = name;
-    this.dbName = snakeCase(name) + 's';
-    this.dbPath = path.join(dataDir, this.dbName + '.db');
-    this.fields = [];
-    this.relations = [];
-    this.methods = [];
+export const assignFunctions = (obj, ...sources) => {
+  return assignWith(obj, ...sources,
+    (objValue, srcValue, key, object, source) => {
+    if (key.startsWith('post')) {
+      const method = key.slice(4)[0].toLowerCase() + key.slice(5);
+      object[method] = (function (fun) {
+        return (...args) => {
+          let res = fun(...args);
+          if (res && res.then) {
+            return res.then(srcValue);
+          } else {
+            return srcValue(res);
+          }
+        }
+      })(object[method])
+    } else if (key.startsWith('pre')) {
 
-    this.emitter = new events.EventEmitter();
-
-    this.field('_id').string();
-  }
-  field(name) {
-    let field = new ModelField(name, this);
-    this.fields.push(field);
-    return field;
-  }
-  oneToMany(manyModel, fieldName) {
-    assert(manyModel !== undefined);
-    assert(manyModel !== {});
-    this.relations.push({
-      model: manyModel,
-      fieldName: fieldName,
-      type: 'oneToMany'
-    });
-    return this;
-  }
-  noDuplicates() {
-    this._noDuplicates = true;
-    return this;
-  }
-  acceptsEmptyQuery() {
-    this.acceptsEmptyQuery = true;
-    return this;
-  }
-  implement(funcName, func) {
-    this.methods.push({
-      name: funcName,
-      callback: func
-    });
-    return this;
-  }
-  hook(evt, callback) {
-    this.emitter.on(evt, callback);
-    return this;
-  }
-
-  done() {
-    let db = undefined, self = this;
-    self.emitter.emit('done:before');
-
-    if (databases[self.dbPath]) {
-      mainStory.warn('db', 'Not loading DB again..');
-      db = databases[self.dbPath];
     } else {
-      db = new Datastore(self.dbPath);
-      db.loadDatabase();
-      databases[self.dbPath] = db;
+      return srcValue ? srcValue : objValue;
     }
+  })
+}
 
-    let model = function (d) {
-      self.emitter.emit('construct:before', this);
-      d = d || {};
-      if (typeof d === 'string') {
-        if (self._default) {
-          let val = d;
-          d = {};
-          d[self._default] = val;
-        } else {
-          throw new Error('Cannot accept a string as an object');
-        }
-      }
-      this.data = {};
-      // let data = Lazy(d).pick(this.fields.map(f => f.name));
-      for (let index in self.fields) {
-        let field = self.fields[index];
-        let value = d[field.name];
-        this.data[field.name] = value || field._defaultValue;
-      }
-      this._id = this.data._id;
-      self.emitter.emit('construct:after', this);
+export const defaultFunctions = (state) => {
+  return Object.assign({}, Object.assign.apply({},
+    ['update', 'set', 'create', 'remove'].map((method) => ({
+      [method]: notImplemented(method)
+    }))
+  ), {
+    getProps() {
+      return Object.assign({}, state._props);
     }
-
-    // We add the custom functions
-    for (let index in self.methods) {
-        let method = self.methods[index];
-        model.prototype[method.name] = method.callback;
+  }, {
+    populate: async () => {
+      state._props = Object.assign({}, await state.db.findOne({
+        _id: state._props._id
+      }));
     }
+  });
+}
+export const getDatabase = (name) => {
 
-    model.model = self;
-    model.prototype.getPayload = function () {
-      self.emitter.emit('getPayload:before', this);
-      let payload = {};
+}
+export const manyToOne = (state, name) => ({
+  postGetProps: ({[name + 'Id']: omit, ...props}) => {
+    return Object.assign({}, props, {
+      [name]: state.populated[name]
+    });
+  },
+  postPopulate: async () => {
+    state.populated[name] = await state.db.find({
+      _id: state._props[name + 'Id']
+    });
+  }
+})
 
-      for (let index in self.fields) {
-        let field = self.fields[index];
-        let value = this.data[field.name];
+export const legacySupport = (state) => ({
+  get data() {
+    return state.functions.getProps();
+  }
+});
 
-        if (typeof value === 'undefined' && field._required)
-          throw new Error(`Field ${field.name} is required`);
-
-        if (field.validator && !field.validator(value)) {
-          if (field._required)
-            throw new Error(`Field ${field.name} is required but has invalid value`);
-          else if (value !== undefined)
-             mainStory.info('db', `Dropping field ${field.name} with ${value}`);
-        } else {
-          payload[field.name] = value;
-        }
-      }
-      self.emitter.emit('getPayload:after', this);
-      return payload;
-    }
-
-    model.prototype.create = async function () {
-      self.emitter.emit('create:before', this);
-      if (this._id || this.data._id)
-        throw new Error('Object is already from database');
-
-      if (self._noDuplicates) {
-        if (!self._default) {
-          throw new Error('No dups specified but no default field');
-        }
-        let q = {};
-        q[self._default] = this.data[self._default];
-        let res = await db.find(q);
-        if (res.length > 0)
-          throw new Error('Entry is already existing and dups found');
-      }
-
-
-     let res = await db.insert(this.getPayload());
-     mainStory.debug('db', `created ${self.name}#${this._id}`);
-
-      this._id = this.data._id = res._id;
-      self.emitter.emit('create:after', this);
-    }
-
-    model.prototype.set = function (key, value) {
-      if (key === '_id') {
-        throw new Error('Cannot set _id');
-      }
-      this.data[key] = value;
-    }
-
-    model.prototype.update = async function (key, value) {
-      self.emitter.emit('update:before', this);
-      if (!this._id) {
-        throw new Error('Cannot update ghost model');
-      }
-
-      await db.update({_id: this._id}, this.getPayload());
-      self.emitter.emit('update:after', this);
-      mainStory.debug('db', `updated ${self.name}#${this._id}`);
+export const updateable = (state) => ({
+  update: async () => {
+    if (!state.dirty) {
+      mainStory.info('db', 'Trying to update a non dirty document');
       return;
     }
-
-    model.findById = async function (id) {
-      return (await model.find({_id: id, limit: 1}))[0];
+    try {
+      let res = await state.db.update({_id: state._props._id}, state._props);
+      return;
+    } catch (err) {
+      throw Boom.wrap(err);
     }
-
-    model.find = async function (query, forceEmpty) {
-      let q = Lazy(query)
-        .omit(self.fields.filter(f => f.notIdentity))
-        .pick(self.fields.map(f => f.name))
-        .value();
-
-      // if (Object.keys(q).length  == 0 && !self.acceptsEmptyQuery
-      //   && !forceEmpty) {
-      //   throw new Error('Empty or invalid query');
-      // }
-
-      let opts = Lazy(query).pick([
-        'limit', 'offset', 'sort', 'direction'
-      ]).value();
-
-      if (!opts.limit || opts.limit > 500 || opts.limit < 1) {
-        opts.limit = 500;
-      }
-
-      let sort = {};
-      sort[opts.sort || 'name'] = opts.direction ?
-        (opts.direction == 'asc' ? 1 : -1) : 1;
-
-      let res = (await db.cfind(q).sort(sort).limit(opts.limit)
-        .skip(opts.skip || 0).exec()).map(d => new model(d));
-
-      res.query = Lazy(q).merge(opts).value();
-      return res;
+  },
+  set: (key, value) => {
+    if (state.fields.includes(key)) {
+      state._props = Object.assign({}, state._props, {[key]: value});
     }
-    // for (let i in this.fields.filter(t => t.type == 'oneToOne')) {
-    //   let field = this.fields[i];
-    //   model.prototype['get' + pascalCase(field.name)] = {
-    //
-    //   }
-    // }
-
-    for (let rel of this.relations) {
-
-      if (rel.type === 'oneToMany') {
-        let m = 'get' + pascalCase(pluralize(rel.model.model.name));
-        model.prototype[m] = function (query) {
-          self.emitter.emit(m + ':before', this);
-          query = query || {};
-          if (!this._id) {
-            throw new Error('Can\'t get children of unserialized object');
-          }
-          query[rel.fieldName] = this._id;
-          self.emitter.emit(m + ':after', this);
-          return rel.model.find(query);
-        }
-      }
-    }
-    return model;
   }
-}
+});
 
-// Chaining example:
-//
-// (new Model('MyModel'))
-//   .field('itemName')
-//     .required()
-//     .string()
-//     .regex(/Regex/)
-//     .done()
-//   .field('itemCategory')
-//     .float()
-//     .notIdentity() <-- means won't be accepted as a query
-//     .done()
-//   .done()
-
-export default Model;
+export const creatable = (state) => ({
+  create: async () => {
+    try {
+      return await db.insert(state._props);
+    } catch (err) {
+      throw Boom.wrap(err);
+    }
+  }
+})
--- src/server/models/Model.test.js
+++ src/server/models/Model.test.js
@@ -1,356 +1,184 @@
-import Model from './Model';
+import {
+    assignFunctions,
+    defaultFunctions,
+    manyToOne,
+    legacySupport,
+    updateable
+  } from './Model';
 import test from 'ava';
-import process from 'process';
+import sinon from 'sinon';
 
-test('constructor should snake case db name', t => {
-  t.is((new Model('myDoc')).dbName, 'my_docs');
-})
-
-test('field should type correctly to float', t => {
-  let m = new Model('myModel');
-  let floatField = m.field('foo').float();
-
-  t.is(floatField.type, 'float');
-
-  t.falsy(floatField.validator('foo'));
-  t.falsy(floatField.validator('e'));
-  t.falsy(floatField.validator());
-  t.falsy(floatField.validator({}));
-  t.falsy(floatField.validator(NaN));
-  t.falsy(floatField.validator(function() {}));
-
-  t.truthy(floatField.validator(15));
-  t.truthy(floatField.validator(42.1));
-  t.truthy(floatField.validator(-1337));
-});
-
-test('field should type correctly to int', t => {
-  let m = new Model('myModel');
-  let intField = m.field('foo').int();
-
-  t.is(intField.type, 'int');
-
-  t.falsy(intField.validator('foo'));
-  t.falsy(intField.validator('e'));
-  t.falsy(intField.validator());
-  t.falsy(intField.validator({}));
-  t.falsy(intField.validator(NaN));
-  t.falsy(intField.validator(function() {}));
-  t.falsy(intField.validator(42.1));
-
-  t.truthy(intField.validator(15));
-  t.truthy(intField.validator(-1337));
-});
-
-test('field should type correctly to string', t => {
-  let m = new Model('myModel');
-  let stringField = m.field('foo').string();
-
-  t.is(stringField.type, 'string');
-
-  t.truthy(stringField.validator('foo'));
-  t.truthy(stringField.validator('e'));
-
-  t.falsy(stringField.validator());
-  t.falsy(stringField.validator({}));
-  t.falsy(stringField.validator(NaN));
-  t.falsy(stringField.validator(function() {}));
-  t.falsy(stringField.validator(42.1));
-});
-
-test('field should type correctly to bool', t => {
-  let m = new Model('myModel');
-  let boolField = m.field('foo').boolean();
-
-  t.is(boolField.type, 'boolean');
-
-  t.falsy(boolField.validator('foo'));
-  t.falsy(boolField.validator());
-  t.falsy(boolField.validator({}));
-  t.falsy(boolField.validator(NaN));
-  t.falsy(boolField.validator(function() {}));
-
-  t.truthy(boolField.validator(true));
-});
-
-test('field should type correctly to any', t => {
-  let m = new Model('myModel');
-  let anyField = m.field('foo').any();
-
-  t.is(anyField.type, 'any');
-
-  t.truthy(anyField.validator('foo'));
-  t.truthy(anyField.validator());
-  t.truthy(anyField.validator({}));
-  t.truthy(anyField.validator(NaN));
-  t.truthy(anyField.validator(function() {}));
-  t.truthy(anyField.validator({
+test('assignFunctions - should assign for normal object', t => {
+  let obj = {
     foo: 'bar',
-    numbers:  [1337, '42'],
-    cat: {dog: {this: {makes: {no: {sense: true}}}}}
-  }));
-  t.truthy(anyField.validator(true));
-  t.truthy(anyField.validator(true));
-});
-
-
-
-test('should chain calls without Error', t => {
-  (new Model('MyModel'))
-    .field('itemName')
-      .required()
-      .string()
-      .done()
-    .field('itemCategory')
-      .float()
-      .notIdentity() // <-- means won't be accepted as a query
-      .done()
-    .noDuplicates()
-    .acceptsEmptyQuery()
-    .done()
-});
-let personModel = (name) => {
-  return (new Model(name))
-    .noDuplicates()
-    .field('name')
-      .required()
-      .string()
-      .defaultParam()
-      .done()
-    .field('age')
-      .int()
-      .done()
-    .done();
-}
-test('model should accept a default type', t => {
-  let Person = personModel('Person');
-
-  t.is((new Person('Danny')).data.name, 'Danny');
-});
-test('model should generate correct payloads', t => {
-
-  let Person = personModel('People');
-  let bilbo = new Person({
-    name: 'Bilbo',
-    age: 131
-  });
-  t.deepEqual(bilbo.getPayload(), {
-    name: 'Bilbo',
-    age: 131
-  });
-
-  t.throws(function () {
-      return (new Person({})).getPayload();
-  });
-
-  t.deepEqual((new Person({
-    name: 'Aragorn',
-    age: 121.2,
-    height: 175
-  })).getPayload(), {
-    name: 'Aragorn'
-  });
-
-  t.throws(function () {
-    (new Person({name: 42})).getPayload();
-  });
-});
-test('model should create() correctly and then find it by id and name', async t => {
-  let Person = personModel('Individuals');
-
-  let bilbo = new Person({
-    name: 'Bilbo',
-    age: 131
-  });
-
-  await bilbo.create();
-
-  let frodo = new Person({name: 'Frodo'});
-  await frodo.create();
-
-  t.not(bilbo._id, undefined);
-
-  let res = await Person.findById(bilbo._id);
-  t.is(res.data.age, 131);
-  t.is(res.data.name, 'Bilbo');
-
-  res = (await Person.find({name: 'Bilbo'}))[0];
-  t.is(res.data._id, bilbo._id);
-  t.is(bilbo.data.age, 131);
-});
-
-test('Model set function', async t => {
-  let Person = personModel('Ppl');
-
-  let jon = new Person({name: 'Jon Snow'});
-
-  await jon.create();
-
-  jon.set('name', 'Jon Targaryen');
-
-  t.is((await Person.findById(jon._id)).data.name, 'Jon Snow');
-
-  await jon.update();
-
-  t.is((await Person.findById(jon._id)).data.name, 'Jon Targaryen');
-});
-
-test('finds multiple docs', async t => {
-  let Person = personModel('Dudes');
-
-  let john = new Person({
-    name: 'john',
-    age: 34
-  });
-
-  await john.create();
-
-  let joe = new Person({
-    name: 'joe',
-    age: 34
-  });
-
-  await joe.create();
-
-  let harvey = new Person({
-    name: 'harvey',
-    age: 42
-  });
-
-  await harvey.create();
-
-  t.is((await Person.find({age: 34})).length, 2);
-  t.is((await Person.find({age: 42})).length, 1);
-});
-
-test('correct dup check', async t => {
-  let Soul = personModel('soul');
-  let joe = new Soul('joe');
-  await joe.create();
-  let joey = new Soul('joe');
-  t.throws(joey.create());
-});
-
-test('oneToMany', async t => {
-
-  let Parent = (new Model('parent'))
-    .field('name')
-      .string()
-      .required()
-      .defaultParam()
-      .done()
-    .field('childId')
-      .string()
-      .done()
-    .done();
-
-  let Child = (new Model('child'))
-    .field('name')
-      .string()
-      .required()
-      .defaultParam()
-      .done()
-    .oneToMany(Parent, 'childId')
-    .done();
-  let bruce = new Child('Bruce');
-  await bruce.create();
-
-  let thomas = new Parent('Thomas');
-  thomas.data.childId = bruce.data._id;
-  await thomas.create();
-
-  let martha = new Parent('Martha');
-  martha.data.childId = bruce.data._id;
-  await martha.create();
-
-  let yourMother = new Parent('Lady of the night'); // :D :D :D
-  await yourMother.create();
-
-  let parents = await bruce.getParents();
-  t.is(parents.length, 2);
-
-  if(parents[0].name == 'Thomas') {
-    t.is(parents[1].data.name, 'Martha')
-    t.is(parents[1].data._id, martha._id);
-    t.is(parents[0].data._id, thomas._id);
-  } else {
-    t.is(parents[1].data.name, 'Thomas')
-    t.is(parents[0].data.name, 'Martha')
-    t.is(parents[1].data._id, thomas.data._id)
-    t.is(parents[0].data._id, martha.data._id);
+    number: 42,
+    nested: {
+      otherNumber: 1337
+    },
+    otherNested: {
+      anwser: 'still 42',
+      sub: {
+        marine: 'true' // lol
+      }
+    }
   }
-
-  // Let's check it support basic query
-  parents = await bruce.getParents({limit: 1});
-  t.is(parents.length, 1);
-  parents = await bruce.getParents({name: /lady/});
-  t.is(parents.length, 0);
-  parents = await bruce.getParents({name: /mas/});
-  t.is(parents.length, 1);
-  t.is(parents[0].data.name, 'Thomas');
+  t.deepEqual(assignFunctions({}, obj, {
+    foo: 'baz',  nested: {  notANumber: NaN  }
+  }, {
+    rule: 34
+  }), {
+    foo: 'baz',  number: 42, nested: {  notANumber: NaN  },
+    rule: 34,  otherNested: {
+      anwser: 'still 42',
+      sub: {  marine: 'true'  }
+    }
+  });
+  t.deepEqual(obj, {
+    foo: 'bar', number: 42, nested: {  otherNumber: 1337},
+    otherNested: {anwser: 'still 42', sub: {  marine: 'true' }}
+  });
 });
 
-test('implements function work', t => {
-  let called = false
+test('assignFunction - should stack post (sync)', t => {
+  let object = {
+    update: (up) => {
+      return 'update';
+    }
+  };
+  let src = {
+    postUpdate: (res) => {
+      return res + ' post';
+    }
+  };
+  let src2 = {
+    postUpdate: (res) => {
+      return res + ' past';
+    }
+  };
+  let obj = assignFunctions({}, object, src, src2);
+  t.is(obj.update(), 'update post past');
+});
 
-  let say = (name, str) => {
-    t.is(name, 'Bacon');
-    t.is(str, 'meoooow');
-    called = true;
-  }
-  let Cat = new Model('cat')
-    .field('name')
-      .required()
-      .string()
-      .defaultParam()
-      .done()
-    .implement('getMeowing', function (length) {
-      return 'me' + 'o'.repeat(length) + 'w';
-    }).implement('meow', function (length) {
-      return say(this.data.name, this.getMeowing(length));
-    }).done();
+test('assignFunctions - should stack post async', async t => {
+  const delay = (ms) => {
+    return new Promise((res) => setTimeout(res, ms));
+  };
+  const resolveAfter = (str, ms) => delay(ms).then(Promise.resolve.bind(Promise, str));
 
-  (new Cat('Bacon')).meow(4);
+  let object = {
+    update: (up) => {
+      return resolveAfter('update', 20);
+    }
+  };
+  let src = {
+    postUpdate: (res) => {
+      return resolveAfter(res + ' post', 20);
+    }
+  };
+  let src2 = {
+    postUpdate: (res) => {
+      return resolveAfter(res + ' past', 20);
+    }
+  };
+  let obj = assignFunctions({}, object, src, src2);
+  t.is(await obj.update(), 'update post past');
+});
+
+test('defaultFunctions - returns standard failing function', t => {
+  let obj = defaultFunctions({});
+  t.truthy(obj.update);
+  t.truthy(obj.set);
+  t.truthy(obj.create);
+  t.truthy(obj.remove);
+  t.throws(() => obj.update());
+  t.throws(() => obj.set());
+  t.throws(() => obj.create());
+  t.throws(() => obj.remove());
+});
+
+test('manyToOne - assigns to passed props populated values', t => {
+  let state = {
+    populated: {
+      sibling: {
+        name: 'Bruce',
+        sex: 'Male'
+      }
+    }
+  };
+  let props = manyToOne(state, 'sibling');
+  let res = props.postGetProps({siblingId: 'id', name: 'Thomas'});
+  t.deepEqual(res, {
+    name: 'Thomas',
+    sibling: {
+      name: 'Bruce',
+      sex: 'Male'
+    }
+  });
+});
+
+test('manyToOne - calls db find and mutate populated values', async t => {
+  let called = false;
+  let state = {
+    populated: {},
+    _props: {
+      fooId: 42
+    },
+    db: {
+      find: (q) => {
+        t.deepEqual(q, {_id: 42});
+        called = true;
+        return Promise.resolve({foo: 'bar', _id: 42});
+      }
+    }
+  };
+  let props = manyToOne(state, 'foo');
+  await props.postPopulate();
   t.truthy(called);
+  t.deepEqual(state.populated, {
+    foo: {foo: 'bar', _id: 42}
+  });
 });
 
-test('hooks', async t => {
-  let barks = 1;
-
-  let hook = function (self) {
-    // console.log(Object.getOwnPropertyNames(self));
-    self.bark();
-  }
-
-  let Dog = new Model('dog')
-    .field('name')
-      .required()
-      .string()
-      .defaultParam()
-      .done()
-    .implement('bark', () => {
-      barks += 1;
-    }).hook('construct:after', (self) => {
-      hook(self);
-    }).hook('create:before', hook)
-    .hook('create:after', hook)
-    .done();
-
-  let Beiley = new Dog('Beiley');
-  // t.is(barks, 2);
-  await Beiley.create();
-  t.is(barks, 4);
+test('legacySupport - calls getProps', t => {
+  let state = {
+    functions : {
+      getProps: () => ({answer: 42})
+    }
+  };
+  t.deepEqual(legacySupport(state).data, {answer: 42});
 });
 
-test('default value', async t => {
-  let Bird = new Model('bird')
-    .field('tweet')
-      .string()
-      .defaultValue('tweet-tweet')
-      .done()
-    .done();
-
-  let sparrow = new Bird();
-  t.is(sparrow.data.tweet, 'tweet-tweet');
-
+test('updateable - update calls db.update', async t => {
+  let callback = sinon.spy();
+  let state = {
+    _props: {
+      _id: 1337,
+      number: 42,
+      foo: 'bar'
+    },
+    dirty: true,
+    db: {update: callback}
+  };
+  updateable(state).update();
+  t.truthy(callback.called);
+  t.truthy(callback.calledWith({_id: 1337}, {
+    _id: 1337,
+    number: 42,
+    foo: 'bar'
+  }));
 });
+
+test('updateable - set mutate props', t => {
+  let state = {
+    _props: {
+      _id: 1337,
+      value: 42
+    },
+    fields: ['value']
+  };
+  updateable(state).set('value', 'foo');
+  t.deepEqual(state._props, {
+      _id: 1337,
+      value: 'foo'
+  });
+})
