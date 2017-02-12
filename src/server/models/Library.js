c007ea5dcb860fec4d78cd1b3442ce4827d73367
vinz243 <vinz243@gmail.com>
2017-02-12 15:29:40 +0100

    refactor: use new model for Library

--- src/server/models/Library.js
+++ src/server/models/Library.js
@@ -1,16 +1,44 @@
-import Model from './Model';
-import Scan from './Scan';
+import {
+  assignFunctions,
+  defaultFunctions,
+  manyToOne,
+  legacySupport,
+  updateable,
+  createable,
+  databaseLoader,
+  publicProps,
+  findOneFactory,
+  findFactory
+} from './Model';
 
-let Library = new Model('library')
-  .field('name')
-    .string()
-    .required()
-    .defaultParam()
-    .done()
-  .field('path')
-    .string()
-    .done()
-  .oneToMany(Scan, 'libraryId')
-  .done();
+export const Library = function(props) {
+  if (typeof props === 'string') {
+    props = {
+      name: props
+    };
+  }
+  let state = {
+    name: 'library',
+    fields: ['name', 'path'],
+    functions: {},
+    populated: {},
+    props
+  };
+  return assignFunctions(
+    state.functions,
+    defaultFunctions(state),
+    updateable(state),
+    createable(state),
+    databaseLoader(state),
+    publicProps(state),
+    legacySupport(state)
+  );
+}
 
-export default Library;
+export const findOne = findOneFactory(Library);
+
+export const findById = (_id) => findOne({
+  _id
+});
+
+export const find = findFactory(Library, 'lîbrary');
