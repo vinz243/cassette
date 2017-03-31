cb39614130dcf4faf2359b2bcb573fedd1679188
vinz243 <vinz243@gmail.com>
2017-03-31 12:34:33 +0200

    feat: album artwork fetch

--- src/server/features/store/controllers/musicbrainz.js
+++ src/server/features/store/controllers/musicbrainz.js
@@ -90,7 +90,7 @@ module.exports = {
       let buffer = null;
 
       if (cache[query]) {
-        
+
         buffer = cache[query];
       } else {
         const url  = 'http://ws.audioscrobbler.com/2.0/?' + query;
@@ -114,6 +114,73 @@ module.exports = {
         const target = getClosestSize(300, availableSizes);
 
         const imageUrl = data.artist.image
+          .find(el => el.size === target)['#text'];
+
+        if (!imageUrl) {
+          return;
+        }
+
+        buffer = await request({
+          url: imageUrl, encoding: null
+        });
+
+        if (keys.length >= MAX_CACHE_SIZE) {
+          delete cache[keys.shift()];
+        }
+
+        keys.push(query);
+        cache[query] = buffer;
+      }
+
+
+      ctx.body = await sharp(buffer).resize(size - 0, size - 0).toBuffer();
+
+      ctx.set('Content-Type', 'image/png');
+      ctx.status = 200;
+    }
+  },
+  '/api/v2/store/release-groups/:mbid/artwork': {
+    get: async function (ctx) {
+      const {size = 300} = ctx.query;
+      const res = await musicbrainz.lookupReleaseGroup(ctx.params.mbid,
+        ['artists']);
+
+      const params = {
+        method: `album.getinfo`,
+        api_key: config.get('lastFMAPIKey'),
+        artist: res.artistCredits[0].artist.name,
+        album: res.title,
+        format: 'json'
+      };
+
+      let query = qs.stringify(params);
+      let buffer = null;
+
+      if (cache[query]) {
+
+        buffer = cache[query];
+      } else {
+        const url  = 'http://ws.audioscrobbler.com/2.0/?' + query;
+
+        const data = await request.get({url, json: true});
+
+        if (data.error) {
+          ctx.status = 500;
+          ctx.body = data;
+          return;
+        }
+
+        const availableSizes = data.album.image.map(s => s.size)
+          .filter(size => (size !== 'mega' && size));
+
+        if (!availableSizes.length) {
+          ctx.status = 404;
+          return;
+        }
+
+        const target = getClosestSize(300, availableSizes);
+
+        const imageUrl = data.album.image
           .find(el => el.size === target)['#text'];
 
         if (!imageUrl) {
